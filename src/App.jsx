168efb0eb019fd084815029c3300d8c8b212987e
tim <tim@timdeMacBook-Pro.local>
2025-03-11 16:49:03 +0800

    practice rwd

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header.jsx'
 import VideoSection from './VideoSection.jsx';
 import Best from './Best.jsx';
+import style from './App.module.css'
 
 function App() {
 
@@ -38,12 +39,18 @@ function App() {
     'image/Shop.jpg',
   ]
   return (
-    <div>
-      <Header />
-      <VideoSection />
-      <Best title="本日最佳推薦" exploreButton="探索全部" images={images} />
-      <Best title="繼續觀賞" exploreButton="歷史紀錄" images={imagesHistory} />
-      <Best title="日本動漫" exploreButton="查看更多" images={imagesJpAnime} />
+    <div className={style.app}>
+      <div className={style.header}>
+        <Header />
+      </div>
+      <div className={style.videoSection}>
+        <VideoSection />
+      </div>
+      <div className={style.best}>
+        <Best title="本日最佳推薦" exploreButton="探索全部" images={images} />
+        <Best title="繼續觀賞" exploreButton="歷史紀錄" images={imagesHistory} />
+        <Best title="日本動漫" exploreButton="查看更多" images={imagesJpAnime} />
+      </div>
     </div>
   );
 }
